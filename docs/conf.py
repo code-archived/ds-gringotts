64212670b6179703fe734a3e68c8e1ebf41b301f
ZenithClown <debmalyapramanik.005@gmail.com>
2024-08-12 23:49:35 +0530

    🎉 add read the docs configurations, make file for sphinx docs generator

---
+++ docs/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+
+# ? insert the project paths to let sphinx recognize/find packages
+sys.path.insert(0, os.path.abspath(os.path.join("..", "modules", "prettify")))
+
+project = 'ds-gringotts'
+copyright = '2024, Debmalya Pramanik'
+author = 'Debmalya Pramanik'
+release = 'v1.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'myst_parser',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary'
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
